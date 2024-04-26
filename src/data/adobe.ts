e88a15b68da9169891d185c034812e979215dbb1
Abhijeet Prasad <aprasad@sentry.io>
2024-04-26 13:54:37 -0400

    feat: Expand adobe domains (#14)

--- src/data/adobe.ts
+++ src/data/adobe.ts
@@ -4,7 +4,16 @@ export const name = "Adobe";
 
 export const statusPageUrl = "https://status.adobe.com/";
 
-export const domains = ["adobe.com", "*.adobe.com"];
+export const domains = [
+  "adobe.com",
+  "*.adobe.com",
+  "adobe.io",
+  "*.adobe.io",
+  "adobecc.com",
+  "*.adobecc.com",
+  "adobe-identity.com",
+  "*.adobe-identity.com",
+];
 
 export const isServiceDefunct = false;
 
