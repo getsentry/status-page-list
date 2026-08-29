f55351594d863f3c6182d04b66b75cda6647e809
iGaming Tools <298231669+igamingtools@users.noreply.github.com>
2026-08-29 13:33:16 +0300

    Add iGaming Tools status page

---
+++ src/data/igaming_tools.ts
@@ -0,0 +1,16 @@
+import { StatusPageInfo } from "../types";
+
+export const name = "iGaming Tools";
+
+export const statusPageUrl = "https://statuspage.i-gaming.tools/";
+
+export const domains = ["i-gaming.tools", "*.i-gaming.tools"];
+
+export const isServiceDefunct = false;
+
+export const iGamingToolsStatusInfo: StatusPageInfo = {
+  name,
+  domains,
+  statusPageUrl,
+  isServiceDefunct,
+};
